9bcde872e92cb2b91cf38d54556dfdaca2c9ea25
Kyle Ondy <kyle@ondy.org>
2023-10-20 21:53:03 -0400

    more bindings

--- nix/modules/hm_modules/dev/clojure.nix
+++ nix/modules/hm_modules/dev/clojure.nix
@@ -70,14 +70,14 @@ in
           {
             plugin = vim-sexp;
             config = ''
-              " set no deafult bindings
+              " TODO: add fennel
               " let g:sexp_filetypes = ""
             '';
           }
 
           ## https://github.com/tpope/vim-sexp-mappings-for-regular-people
           ## tpope to the rescue again
-          #{ plugin = vim-sexp-mappings-for-regular-people; }
+          { plugin = vim-sexp-mappings-for-regular-people; }
         ];
       };
     };
