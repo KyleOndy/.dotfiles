637132449ee50194f2cb9e9fec183598d246e0d7
Kyle Ondy <kyle@ondy.org>
2024-02-11 15:34:06 -0500

    wip

---
+++ nix/pkgs/my-scripts/scripts/foundry/README.md
@@ -0,0 +1,17 @@
+# Foundry
+
+Mainly used to manage my day-to-day at ${WORK}. This is not intended to be
+reusable, I write and maintain this functionality for myself. Feel free to take
+what you from this.
+
+## functionality
+
+### Logs
+
+My main use of `foundry` is to make ad-hoc notes throughout the day, and almost
+more importantly, give me the tools and a framework for reviewing and
+propagating these notes over the period of weeks, quarters, and years.
+
+### Work tracking
+
+Foundry can open a new `tmux` window setup for a specific ticket.
---
+++ nix/pkgs/my-scripts/scripts/foundry/foundry.clj
@@ -0,0 +1,33 @@
+(comment`
+  foo
+
+(str (java.time.LocalDate/of 2019 12 18))
+
+
+(str (java.time.LocalDate/now))
+
+(java.time.LocalDate/now)
+
+
+
+; date formats: https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+
+(.format
+(java.time.LocalDate/now)
+(java.time.format.DateTimeFormatter/ofPattern "yyyy-MM-dd"))
+
+(.format
+(java.time.LocalDate/now)
+(java.time.format.DateTimeFormatter/ofPattern "w"))
+
+(.format
+(java.time.LocalDate/now)
+(java.time.format.DateTimeFormatter/ofPattern "q"))
+
+(week-of-date
+(java.time.LocalDate/now))
+)
+
+(defn week-of-date [date] (
+(str (.format date (java.time.format.DateTimeFormatter/ofPattern "w"))
+  )))
