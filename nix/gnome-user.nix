7b0eaedb69e0ccf761f77cd793a9049037d1410d
Kyle Ondy <kyle@ondy.org>
2024-02-13 16:50:51 -0500

    copying https://hoverbear.org/blog/declarative-gnome-configuration-in-nixos/

--- nix/gnome-user.nix
+++ nix/gnome-user.nix
@@ -33,4 +33,46 @@
   };
 
   home.sessionVariables.GTK_THEME = "palenight";
+
+  # Use `dconf watch /` to track stateful changes you are doing, then set them here.
+  dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "code.desktop"
+        "org.gnome.Terminal.desktop"
+        "spotify.desktop"
+        "virt-manager.desktop"
+        "org.gnome.Nautilus.desktop"
+      ];
+      disable-user-extensions = false;
+
+      # `gnome-extensions list` for a list
+      enabled-extensions = [
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+        "trayIconsReloaded@selfmade.pl"
+        "Vitals@CoreCoding.com"
+        "dash-to-panel@jderose9.github.com"
+        "sound-output-device-chooser@kgshank.net"
+        "space-bar@luchrioh"
+      ];
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      workspace-names = [ "Main" ];
+    };
+  };
+
+  home.packages = with pkgs; [
+    # ...
+    gnomeExtensions.user-themes
+    gnomeExtensions.tray-icons-reloaded
+    gnomeExtensions.vitals
+    gnomeExtensions.dash-to-panel
+    gnomeExtensions.sound-output-device-chooser
+    gnomeExtensions.space-bar
+  ];
 }
